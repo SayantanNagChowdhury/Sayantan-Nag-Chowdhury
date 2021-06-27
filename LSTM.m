422625047dfe05f0dfd4be8b72370c0068b4d3bb
SayantanNagChowdhury <86559029+SayantanNagChowdhury@users.noreply.github.com>
2021-06-27 18:48:43 +0530

    Add files via upload

---
+++ LSTM.m
@@ -0,0 +1,89 @@
+# LSTM 
+ 
+x=load('ts_peak.dat');
+
+data=[x(:,2)]';
+
+numTimeStepsTrain = floor(0.9*numel(data));
+dataTrain = data(1:numTimeStepsTrain+1);
+dataTest = data(numTimeStepsTrain+1:end);
+
+mu = mean(dataTrain);
+sig = std(dataTrain);
+
+dataTrainStandardized = (dataTrain - mu) / sig;
+
+XTrain = dataTrainStandardized(1:end-1);
+YTrain = dataTrainStandardized(2:end);
+
+numFeatures = 1;
+numResponses = 1;
+numHiddenUnits = 200;
+
+layers = [ ...
+    sequenceInputLayer(numFeatures)
+    lstmLayer(numHiddenUnits)
+    fullyConnectedLayer(numResponses)
+    regressionLayer];
+    
+    options = trainingOptions('adam', ...
+    'MaxEpochs',250, ...
+    'GradientThreshold',1, ...
+    'InitialLearnRate',0.005, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropPeriod',125, ...
+    'LearnRateDropFactor',0.2, ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+    
+net = trainNetwork(XTrain,YTrain,layers,options);
+ 
+dataTestStandardized = (dataTest - mu) / sig;
+XTest = dataTestStandardized(1:end-1);
+
+net = predictAndUpdateState(net,XTrain);
+[net,YPred] = predictAndUpdateState(net,YTrain(end));
+
+numTimeStepsTest = numel(XTest);
+
+for i = 2:numTimeStepsTest
+    [net,YPred(:,i)] = predictAndUpdateState(net,YPred(:,i-1),'ExecutionEnvironment','cpu');
+end
+
+ YPred = sig*YPred + mu;
+ 
+ YTest = dataTest(2:end);
+ rmse = sqrt(mean((YPred-YTest).^2))
+
+net = resetState(net);
+net = predictAndUpdateState(net,XTrain);
+
+YPred = [];
+numTimeStepsTest = numel(XTest);
+for i = 1:numTimeStepsTest
+    [net,YPred(:,i)] = predictAndUpdateState(net,XTest(:,i),'ExecutionEnvironment','cpu');
+end
+
+YPred = sig*YPred + mu;
+
+rmse = sqrt(mean((YPred-YTest).^2))
+mse = mean((YPred-YTest).^2)
+mse_1= mean((mean(YTest)-YTest).^2)
+R_2=1.0-(mse/mse_1)
+
+figure
+plot(YTest)
+hold on
+plot(YPred,'.-')
+hold off
+legend(["Observed" "Predicted"])
+ylabel("Events")
+title("Forecast with Updates")
+
+A=YTest';
+B=YPred';
+C=[A B];
+
+dlmwrite('extreme_predict.txt',C,'delimiter','\t')
+
+
